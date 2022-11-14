45bd966f31762f8cefba6ab878342bab71a6968b
Aristide910 <58232272+Aristide910@users.noreply.github.com>
2022-11-14 00:02:47 -0800

    Add files via upload

---
+++ clone_site.py
@@ -0,0 +1,12 @@
+from pywebcopy import save_website
+
+save_website(
+    url="http://dbei.innov.intraorange/",
+    project_folder="C:/Users/CWYK7024/Documents/clone",
+    project_name="DBEI_clone",
+    bypass_robots=True,
+    debug=True,
+    open_in_browser=True,
+    delay=None,
+    threaded=False,
+)
